5e76c49e2ef508205eefa109a0c8126dc713b435
Nihal Soans <nihalsoans91@gmail.com>
2018-03-15 19:21:37 -0400

    Add files via upload

--- shell/file-reorder.sh
+++ shell/file-reorder.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-i=1; temp=$(mktemp -p .); for file in left*
+curdir=${PWD##*/}
+i=1; temp=$(mktemp -p .); for file in image*
 do
 mv "$file" $temp;
-mv $temp $(printf "image_%0.3d.jpg" $i)
+mv $temp $(printf "img_%s_%0.3d.jpg" $curdir $i)
 i=$((i + 1))
 done
+rm file-reorder.sh
