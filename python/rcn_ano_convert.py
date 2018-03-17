a0af73484f1fec36b26215dc3a1db2f39fe61d7c
Nihal Soans <nihalsoans91@gmail.com>
2018-03-16 20:23:59 -0400

    Update rcn_ano_convert.py

--- python/rcn_ano_convert.py
+++ python/rcn_ano_convert.py
@@ -6,7 +6,6 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 ano=os.path.join(dir_path,"Annotations")
 dfiles=os.listdir(os.path.join(dir_path,"Annotations"))
 for files in dfiles:
-    #print(os.path.join(ano,files))
 
     mydoc = minidom.parse(os.path.join(ano,files))
     clas=mydoc.getElementsByTagName('name')
@@ -18,7 +17,7 @@ for files in dfiles:
 
     x2=polygon[0].childNodes[1].childNodes[0].firstChild.data
     y2=polygon[0].childNodes[1].childNodes[1].firstChild.data
-    #print(os.path.join(ano,files)+","+str(x1)+","+str(y2)+","+str(x2)+","+str(y2)+","+c)
+    
     f.write(os.path.join(ano,files)+","+str(x1)+","+str(y2)+","+str(x2)+","+str(y2)+","+c+"\n")
 
 f.close
